0ccb03f3b4d5b42db2df322345c5875230cf270e
Nguyen Hoang Thinh <nhthinh@live.com>
2021-07-18 22:03:38 +0700

    v

--- DAK_MVC/js/addpost.js
+++ DAK_MVC/js/addpost.js
@@ -1,28 +1,30 @@
 ﻿function AddPost() {
   
-    var surl = GetURL('newpost'); //  arr.join('/') + 'layouthtml/newpost.aspx';
-    $.ajax({
-        url: surl,
-        type: "GET",
-        cache: false,
-        success: function (content) {
-            var opt = [{
-                name: "Tiếp tục", fClick: function () {
-                    ClosePopup();
-                }
-            },{
-                name: "Hủy", fClick: function () {
-                    ClosePopup();
-                    }, className: "button-light"
-            }];
-            ShowPopup('Đăng tin', content, opt);
-            // dang ky event
-            $('#txtDiachi').blur(function () { alert();});
-        },
-        error: function (e) {
+    //var surl = GetURL('newpost'); //  arr.join('/') + 'layouthtml/newpost.aspx';
+    //$.ajax({
+    //    url: surl,
+    //    type: "GET",
+    //    cache: false,
+    //    success: function (content) {
+    //        var opt = [{
+    //            name: "Tiếp tục", fClick: function () {
+    //                ClosePopup();
+    //            }
+    //        },{
+    //            name: "Hủy", fClick: function () {
+    //                ClosePopup();
+    //                }, className: "button-light"
+    //        }];
+    //        ShowPopup('Đăng tin', content, opt);
+    //        // dang ky event
+    //        $('#txtDiachi').blur(function () { alert();});
+    //    },
+    //    error: function (e) {
 
-        }
-    });
+    //    }
+    //});
+  
+    window.locationbar.href = '/DAK/NewPost';
  
 }
 
--- DAK_MVC/js/master.js
+++ DAK_MVC/js/master.js
@@ -33,7 +33,28 @@ function HideShowMenuBarMaster() {
 }
 
 function GetURL(key) {
-    var arr = window.location.href.split('/');
-    arr[arr.length - 1] = '';
-    return arr.join('/') + 'layouthtml/'+key+'.aspx';
+ 
+
+    return getBaseURL() + key;
+}
+function getBaseURL() {
+    var url = location.href;  // entire url including querystring - also: window.location.href;
+    var baseURL = url.substring(0, url.indexOf('/', 14));
+
+
+    if (baseURL.indexOf('http://localhost') != -1) {
+        // Base Url for localhost
+        var url = location.href;  // window.location.href;
+        var pathname = location.pathname;  // window.location.pathname;
+        var index1 = url.indexOf(pathname);
+        var index2 = url.indexOf("/", index1 + 1);
+        var baseLocalUrl = url.substr(0, index2);
+
+        return baseLocalUrl + "/";
+    }
+    else {
+        // Root Url for domain name
+        return baseURL + "/";
+    }
+
 }
